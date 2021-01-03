57767506560e09ac3ffe05c363e44c45389d9e64
Abir Hasan <hackitabirhasan@gmail.com>
2021-01-03 22:23:12 +0600

    P1

--- translation.py
+++ translation.py
@@ -4,10 +4,8 @@ This is a Telegram URL Upload Bot!
 
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
 
-/help for more details..
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe , @TGBotsZ & @CWProjects"""
+/help for more details.
+"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
